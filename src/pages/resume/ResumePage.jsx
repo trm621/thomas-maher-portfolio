adf4bf8bb49729d24922eff4ae4978e56c2c73c2
trm621 <t.maher621@gmail.com>
2026-02-24 18:26:06 -0500

    simplify logic, add styling and correct resume to resume page

--- src/pages/resume/ResumePage.jsx
+++ src/pages/resume/ResumePage.jsx
@@ -1,10 +1,14 @@
-import respdf from "../../assets/ThomasMaherResume.pdf";
+import respdf from "../../assets/ThomasMaherResume2025.pdf";
 import "./ResumePage.css";
 
 const skills = [
+  "Software Development Lifecycle (SDLC)",
+  "Project Management (Agile/Scrum)",
+  " Cross-Functional Collaboration",
   "JavaScript",
   "Angular",
   "Python",
+  "Typescript",
   "Ember",
   "React",
   "Third-Party/Server-Side APIs",
@@ -18,18 +22,18 @@ const skills = [
 function Resume() {
   return (
     <div>
-      <h1 className="welcomeAbout">Technical Skills</h1>
-      <ol className="skillList">
-        {skills.map((skill) => (
-          <li>{skill}</li>
-        ))}
-      </ol>
       <h1 className="resume">
         <a href={respdf} target="_blank" rel="noreferrer">
           {" "}
           Click here to download my resume!
         </a>
       </h1>
+      <h1 className="welcomeAbout">Key Skills</h1>
+      <ol className="skillList">
+        {skills.map((skill) => (
+          <li>{skill}</li>
+        ))}
+      </ol>
     </div>
   );
 }
