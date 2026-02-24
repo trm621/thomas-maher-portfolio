ed35e450f6c6e437ada5dfe94304ff8a08256129
trm621 <t.maher621@gmail.com>
2026-02-23 22:29:53 -0500

    set up header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Thomas Maher's Portfolio</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import './HomePage.css';
+
+export function HomePage() {
+  return (
+    <>
+      <Header />
+      <title>Thomas Maher Portfolio</title>
+      <div className="home-page">
+        
+      </div>
+    </>
+  );
+}
